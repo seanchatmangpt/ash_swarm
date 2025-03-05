c4ede43a6f5052a83c1bc56169aa1b26f1ed7385
jmanhype <straughterguthrie@gmail.com>
2025-03-05 16:12:24 -0600

    Disable warnings-as-errors for test environment

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule AshSwarm.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
+      elixirc_options: [warnings_as_errors: Mix.env() != :test],
       start_permanent: Mix.env() == :prod,
       consolidate_protocols: Mix.env() != :dev,
       aliases: aliases(),
